f3bd597c09159e7c6f38bdb3ba7f237f59f13bb5
Bruno Oliveira e Oliveira <brunooliveiraeoliveira@outlook.com>
2018-03-30 10:43:20 -0300

    add function ordenarQtt in ordenacoes.js

--- javascript/ordenacoes.js
+++ javascript/ordenacoes.js
@@ -1,1 +1,34 @@
-var dados = "56;5;7;1;2;3;8;54,4;6;86;4";
+var dados = "5;9;1;-89;69856;3;5;8;1;5;7;8;2;3;2;5;2";
+
+function ordenarQtt(dados){
+    var ordenado = false;
+    dados = dados.split(";");
+
+    for (var i = 0;i < dados.length; i++) {
+        dados[i] = parseInt(dados[i]);
+    }
+
+    while (!ordenado) {
+        var aux;
+
+        for (var i = 0; i < dados.length; i++) {
+            if (i == (dados.length-1)) {
+                ordenado = true;
+                break;
+            }
+
+            if (dados[i] > dados[i+1]) {
+                aux = dados[i+1];
+                dados[i+1] = dados[i];
+                dados[i] = aux;
+                break;
+            }
+        }
+    }
+
+
+    document.write(dados);
+}
+
+
+ordenarQtt(dados);
